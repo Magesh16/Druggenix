f889c286b285b956656d5c7842493b8902b94319
Magesh16 <mageshsk16@gmail.com>
2021-07-09 19:28:33 +0530

    Add Play/Pause functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./styles/app.scss";
 import Song from "./components/Song";
-import Player from "./components/Player"
+import Player from "./components/Player";
 import data from "./data";
 
 function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
-
+  const [isPlayiing, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
-      <Song currentSong={currentSong}/>
-      <Player currentSong={currentSong}/>
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlayiing}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
