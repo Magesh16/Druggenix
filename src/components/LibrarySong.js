efe1a22048e2a44f2647e6e09ef3cd39d4026c9f
Magesh16 <mageshsk16@gmail.com>
2021-07-11 20:48:16 +0530

    Add active songs

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -6,30 +6,49 @@ const LibrarySong = ({
   setCurrentSong,
   id,
   audioRef,
-  isPlaying
-  
+  isPlaying,
+  setSongs,
 }) => {
   const songSelectHandler = () => {
     const selectedSong = songs.filter((state) => state.id === id);
     setCurrentSong(selectedSong[0]);
-    
+
+    //Add Active state 
+    const newSongs = songs.map((song)=>{
+        if(song.id===id){
+            return{
+                ...song,
+                active:true,
+            }
+        }else{
+            return{
+                ...song,
+                active:false,
+            }
+        }
+    })
+
+    setSongs(newSongs);
+
     //check if the song is isPlaying
 
-    if(isPlaying){
-        const playPromise = audioRef.current.play();
-        if(playPromise !== undefined){
-            playPromise.then((audio) =>{
-                audioRef.current.play();
-            })
-        }
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
     }
   };
   return (
-    <div onClick={songSelectHandler} className="library-song">
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}>
       <img alt={song.name} src={song.cover}></img>
       <div className="song-description">
         <h3>{song.name}</h3>
-        <h4>{song.artist}</h4>
+        <h4>{song.artist}</h4>  
       </div>
     </div>
   );
